284418105fc883e7f873cbe807b881001172fb49
WitchSeele <352081652@qq.com>
2021-09-05 16:28:01 +0800

    first

---
+++ src/main/java/com/example/communitymanager/CommunityManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.communitymanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunityManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/communitymanager/CommunityManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.communitymanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
