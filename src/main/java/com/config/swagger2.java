decb53bc7f86392dc31a50ff56a1b60de823ad3c
WitchSeele <352081652@qq.com>
2021-09-07 15:53:23 +0800

    origin

---
+++ src/main/java/com/config/swagger2.java
@@ -0,0 +1,50 @@
+package com.config;
+
+import io.swagger.models.Contact;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Parameter;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+@EnableSwagger2
+public class swagger2 {
+
+    @Bean
+    public Docket createRestApi() {
+        ParameterBuilder parameterBuilder = new ParameterBuilder();
+        List<Parameter> parameters = new ArrayList<Parameter>();
+        parameterBuilder.name("accessToken").description("Token令牌")
+                .modelRef(new ModelRef("string")).parameterType("header").required(false).build();
+        parameters.add(parameterBuilder.build());
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+//                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
+                .paths(PathSelectors.any())
+                .build()
+                .globalOperationParameters(parameters);
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("社区管理系统")
+                .description("社区管理系统后台api接口文档")
+               // .contact(new contact"WitchSeele")
+                .version("1.0")
+                .build();
+    }
+}
